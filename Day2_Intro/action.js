260585562c938a9f40ae81b2009340fe6819531b
Hareesh <hareesh.madinedi@gmail.com>
2025-12-16 13:44:41 +0530

    Day2_intro

---
+++ Day2_Intro/action.js
@@ -0,0 +1,12 @@
+console.log('hello world');
+
+console.log('hello world');
+let btn=document.getElementsByClassName('btn')
+
+ for(let i=0;i<btn.length;i++){
+     btn[i].addEventListener('click',()=>{
+    alert('item added to cart')
+    })
+}
+
+console.log('hello world');
